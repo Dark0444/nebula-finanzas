4e4acb755c9f875847d3da6220d3e56ca4d95815
Dark0444 <147225219+Dark0444@users.noreply.github.com>
2026-09-20 11:01:26 -0600

    Subida icons apuntando al index

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@
 */
 const CACHE = 'nebula-shell-v1';
 const APP_SHELL = ['./', './index.html', './manifest.json',
-  './icons/icon-192.png', './icons/icon-512.png', './icons/icon-maskable-512.png'];
+  './icon-192.png', './icon-512.png', './icon-maskable-512.png'];
 
 self.addEventListener('install', evt=>{
   self.skipWaiting();
